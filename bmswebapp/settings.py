f19a25fe98091f499bae554a5db856fbff49c41d
vickvey <vivek22@iiserb.ac.in>
2024-07-31 01:10:41 +0530

    added some css functionality and styling

--- bmswebapp/settings.py
+++ bmswebapp/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os, json
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Build paths inside the project like this: BASE_DIR / 'subdir'. 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
